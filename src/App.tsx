0ef74969b6fca1bfb48b1c5b42fa50b96b0bc525
Sebastian Segura <segurasebastian7887@gmail.com>
2019-07-12 14:08:26 -0300

    Add Router Transition

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Switch,
   RouteComponentProps
 } from 'react-router-dom';
+import { AnimatedSwitch, spring } from 'react-router-transition';
 import ChatListScreen from './components/ChatListScreen';
 import ChatRoomScreen from './components/ChatRoomScreen';
 
@@ -14,12 +15,28 @@ const matchComponent = ({ match }: RouteComponentProps<{ chatId: string }>) => (
   <ChatRoomScreen chatId={match.params.chatId} />
 );
 
+const glide = (val: number) =>
+  spring(val, {
+    stiffness: 174,
+    damping: 24
+  });
+
+const mapStyles = (styles: any) => ({
+  transform: `translateX(${styles.offset}%)`
+});
+
 const App: React.FC = () => (
   <Router>
-    <Switch>
+    <AnimatedSwitch
+      atEnter={{ offset: 100 }}
+      atLeave={{ offset: glide(-100) }}
+      atActive={{ offset: glide(0) }}
+      mapStyles={mapStyles}
+      className="switch-wrapper"
+    >
       <Route exact path="/chats" component={ChatListScreen} />
       <Route exact path="/chats/:chatId" component={matchComponent} />
-    </Switch>
+    </AnimatedSwitch>
     <Route exact path="/" component={redirectToChats} />
   </Router>
 );
